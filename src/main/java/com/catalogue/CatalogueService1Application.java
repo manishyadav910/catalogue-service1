b76a5d271b3bf20ca81947f813a00a9a230ce59c
manishyadav910 <manishyadav910@gmail.com>
2022-09-14 23:36:58 +0530

    Add files via upload
    skeleton commit

---
+++ src/main/java/com/catalogue/CatalogueService1Application.java
@@ -0,0 +1,13 @@
+package com.catalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogueService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogueService1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/manish/demo/CatalogueService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manish.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogueService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
